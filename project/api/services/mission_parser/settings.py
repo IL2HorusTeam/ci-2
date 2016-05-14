e151ec8c4a6ae56b1b75ce1c6ca244b5dac2dbae
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-05-14 15:38:30 +0300

    add support for uploads to google drive

--- project/api/services/mission_parser/settings.py
+++ project/api/services/mission_parser/settings.py
@@ -8,24 +8,35 @@ from project.app import app
 env = os.getenv
 
 
+ALLOWED_EXTENSIONS = ['mis', ]
+ALLOWED_MIME_TYPES = ['application/octet-stream', ]
+
+
 # -----------------------------------------------------------------------------
 # Github
 # -----------------------------------------------------------------------------
 
-_github_report_issues = env('EVENTS_PARSER_GITHUB_REPORT_ISSUES')
+_github_report_issues = env('MISSION_PARSER_GITHUB_REPORT_ISSUES')
 GITHUB_REPORT_ISSUES = (
     int(_github_report_issues) == 1
     if _github_report_issues is not None else
     app.config['DEFAULT_GITHUB_REPORT_ISSUES'])
 
 GITHUB_REPO_OWNER = env(
-    'EVENTS_PARSER_GITHUB_REPO_OWNER',
+    'MISSION_PARSER_GITHUB_REPO_OWNER',
     app.config['DEFAULT_GITHUB_REPO_OWNER'])
 
 GITHUB_ACCESS_TOKEN = env(
-    'EVENTS_PARSER_GITHUB_ACCESS_TOKEN',
+    'MISSION_PARSER_GITHUB_ACCESS_TOKEN',
     app.config['DEFAULT_GITHUB_ACCESS_TOKEN'])
 
 GITHUB_REPO_NAME = env(
-    'EVENTS_PARSER_GITHUB_REPO_NAME',
-    'il2fb-events-parser')
+    'MISSION_PARSER_GITHUB_REPO_NAME',
+    'il2fb-mission-parser')
+
+
+# -----------------------------------------------------------------------------
+# Google Drive
+# -----------------------------------------------------------------------------
+
+GOOGLE_DRIVE_FOLDER_ID = env('MISSION_PARSER_GOOGLE_DRIVE_FOLDER_ID')
--- project/settings.py
+++ project/settings.py
@@ -1,19 +1,44 @@
 # coding: utf-8
 
 import os
+import tempfile
+
 
 env = os.getenv
 
-#------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------
 # Flask
-#------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
 
 DEBUG = int(env('FLASK_DEBUG', 0)) == 1
 SECRET_KEY = env('FLASK_SECRET_KEY', '^vl#9lg@xm1@+6s*fb!lym0l#n#v5&6xp')
 
-#------------------------------------------------------------------------------
-# Github
-#------------------------------------------------------------------------------
+MAX_CONTENT_LENGTH = 1 * 1024 * 1024  # 1 MiB for uploads
+UPLOAD_FOLDER = os.path.join(tempfile.gettempdir(), 'uploads')
+if not os.path.exists(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
 
-DEFAULT_GITHUB_REPO_OWNER = 'IL2HorusTeam'
+
+# -----------------------------------------------------------------------------
+# Github
+# -----------------------------------------------------------------------------
+
+DEFAULT_GITHUB_REPO_OWNER = env('DEFAULT_GITHUB_REPO_OWNER', 'IL2HorusTeam')
 DEFAULT_GITHUB_REPORT_ISSUES = int(env('DEFAULT_GITHUB_REPORT_ISSUES', 0)) == 1
+DEFAULT_GITHUB_ACCESS_TOKEN = env('DEFAULT_GITHUB_ACCESS_TOKEN', None)
+
+
+# -----------------------------------------------------------------------------
+# Google Drive
+# -----------------------------------------------------------------------------
+
+GOOGLE_SERVICE_EMAIL = env('GOOGLE_SERVICE_EMAIL')
+GOOGLE_SERVICE_PRIVATE_KEY = env('GOOGLE_SERVICE_PRIVATE_KEY')
+GOOGLE_SERVICE_CREDENTIALS_STORAGE = os.path.join(
+    tempfile.gettempdir(),
+    'google-service-credentials.json')
+
+GOOGLE_DRIVE_API_NAME = 'drive'
+GOOGLE_DRIVE_API_VERSION = 'v2'
+GOOGLE_DRIVE_API_SCOPE = 'https://www.googleapis.com/auth/drive.file'
